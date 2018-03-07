3a3cf47d22980e8e90e99d6ae16210cdcd796257
Egor <malyutinea@inbox.ru>
2018-03-07 22:50:58 +0600

    upd

--- check.go
+++ check.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"net"
+	"time"
+)
+
+const (
+	DEFAULT_TIMEOUT = time.Duration(5 * time.Second)
+)
+
+func Fetch(link string) string {
+	resp, err := net.DialTimeout("tcp", link, DEFAULT_TIMEOUT)
+	if err == nil {
+		resp.Close()
+		return GenerateUpJSON()
+	} else {
+		return GenerateDownJSON()
+	}
+}
+
 func Check(link string) string {
 	link = ValidateURL(link)
 
